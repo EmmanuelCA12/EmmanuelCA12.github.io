5ce4f0e0f8b9b0712bf1d233935695b4847a8194
EmmanuelCA12 <85908695+EmmanuelCA12@users.noreply.github.com>
2024-06-15 09:53:36 -0600

    1

--- Hola-mundo.js
+++ Hola-mundo.js
@@ -3,4 +3,4 @@ alert("¿Cómo has estado?");
 alert("Tal vez no sepas quien habla");
 alert("O tal vez lo hayas olvidado");
 document.write("Hola bienvenidos desde la fuente de JS");
-alert("Mocachino");
+console.log("Muestra esto en la consola");
