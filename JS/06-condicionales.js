3aa9f91e13cc2590174d9d176e0c1a6e5fd465b7
EmmanuelCA12 <85908695+EmmanuelCA12@users.noreply.github.com>
2024-06-15 19:30:28 +0000

    465

---
+++ JS/06-condicionales.js
@@ -0,0 +1,12 @@
+'use strict'
+
+//Condicional IF
+//Si A es Igual a B haz esto
+var edad1=20;
+var edad2=35;
+    //Si pasa esto
+if (edad1 > edad2) {
+    //Se ejecuta esto
+    console.log("La edad 1 es mayor que la edad 2");
+}
+    console.log("Edad 2 es mayor que edad 1");
