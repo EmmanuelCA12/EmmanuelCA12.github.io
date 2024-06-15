3b5138f802b948a4ef90122a7ac6fb6bb867b778
EmmanuelCA12 <85908695+EmmanuelCA12@users.noreply.github.com>
2024-06-15 19:16:49 +0000

    852

--- JS/05-tipos-de-datos.js
+++ JS/05-tipos-de-datos.js
@@ -7,3 +7,10 @@ var operacion=numero1+numero2
 
 alert("El resultado de la operación es: "+operacion);
 //Tipos de datos
+var numero_entero=44;
+var cadena_de_texto="Hola mundo";
+var verdadero_o_falso= true;
+
+var numero_falso="33";
+
+Number (numero_falso);
