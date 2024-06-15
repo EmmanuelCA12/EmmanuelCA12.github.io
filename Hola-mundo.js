11d63ad69040d65598f88447ca08d1f0e527ae40
EmmanuelCA12 <85908695+EmmanuelCA12@users.noreply.github.com>
2024-06-15 09:44:10 -0600

    Cambios de pagina web

---
+++ Hola-mundo.js
@@ -0,0 +1,6 @@
+alert("Hola bebé");
+alert("¿Cómo has estado?");
+alert("Tal vez no sepas quien habla");
+alert("O tal vez lo hayas olvidado");
+document.write("Hola bienvenidos desde la fuente de JS");
+alert("Mocachino");
