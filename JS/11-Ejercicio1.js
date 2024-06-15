19fa17fa1e21980e8004aea87f611f08d87a7d0f
EmmanuelCA12 <85908695+EmmanuelCA12@users.noreply.github.com>
2024-06-15 21:23:53 +0000

    Submit

--- JS/11-Ejercicio1.js
+++ JS/11-Ejercicio1.js
@@ -2,13 +2,14 @@
 /*
 Que pida dos números y que nos diga cual es el mayor y cual es el menor y si son iguales.
 */
-var numero1 = prompt("Ingresa el primer número");
-var numero2 = prompt("Ingresa el segundo número");
+var nombre1 = String(prompt("Ingresa tu nonbre y primer apellido (iniciando con mayuculas cada palabra)"));
+//var apellido = String(prompt("Ingresa tu apellido"));
 
-if (numero1 > numero2) {
-    alert("El primer número "+numero1+" es mayor que el número "+numero2 );
-}else if (numero2 > numero1) {
+if (nombre1 = "Hector Garcia") {
+    alert("¿Unos besos o que compadre?" );
+/*}else if (numero2 > numero1) {
     alert("El segundo número "+numero2+" es mayor que el número "+numero1);
 }else if (numero1 == numero2) {
     alert("Los dos números son iguales");
 }
+    */
