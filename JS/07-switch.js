f676629de498a222a1e506c7185a54e474d8153a
EmmanuelCA12 <85908695+EmmanuelCA12@users.noreply.github.com>
2024-06-15 19:42:12 +0000

    1258

---
+++ JS/07-switch.js
@@ -0,0 +1,23 @@
+'use strict'
+//Switch
+
+var edad=18;
+var imprime=" ";
+
+switch (edad) {
+    case 18:
+        imprime("Acabas de cumplir la mayoria de edad");
+        break;
+    case 25:
+        imprime("ya eres un adulto");
+        break;    
+    case 40:
+        imprime("Ya estas ruco");
+        break;
+    case 75:
+        imprime("Ya muerete, desperdicias oxigeno");
+        break;
+    default:
+        imprime("Tu edad es neutra");
+        break;
+}
