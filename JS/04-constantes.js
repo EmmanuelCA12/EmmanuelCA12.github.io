0a802714d8600e2d3ef94e1bd8504dc0fb3b01bb
EmmanuelCA12 <85908695+EmmanuelCA12@users.noreply.github.com>
2024-06-15 19:01:06 +0000

    123

---
+++ JS/04-constantes.js
@@ -0,0 +1,11 @@
+'use strict'
+
+//Constantes
+//Como un variable pero su valor no puede cambiar
+
+var web="www.google.com";
+const ip= "192.154.54.25";
+
+web="www.youtube.com";
+
+console.log(web, ip);
