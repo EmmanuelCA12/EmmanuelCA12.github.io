56635fc2db0624c9888ebaec88cd271c6650c972
EmmanuelCA12 <85908695+EmmanuelCA12@users.noreply.github.com>
2024-06-15 21:40:48 +0000

    Submit

---
+++ JS/12-Ejercicio2.js
@@ -0,0 +1,23 @@
+'use strict'
+
+/*
+Utilizando un bucle mostrar la media y la suma de los resultados hasta ingresar un 
+número negativo y ahi mostrar el resultado
+*/
+var suma = 0;
+var contador = 0;
+
+do {
+    var numero = ParseInt(prompt("Introduce números hasta que sea uno negativo",0));
+    if (isNaN(numero)) {
+        numero=0;
+    }else if (numero >= 0) {
+        suma = suma + numero;
+
+        contador++;
+    }
+    console.log(suma);
+    console.log(contador);
+} while (numero >= 0);
+alert("La suma de todos los números es: "+suma);
+alert("La media de todos los números es: "+(suma/contador));
