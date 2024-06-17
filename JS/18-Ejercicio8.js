287ee43a73378437c5daff2753e2abc3ca05f97a
EmmanuelCA12 <85908695+EmmanuelCA12@users.noreply.github.com>
2024-06-17 19:44:13 +0000

    Submit

--- JS/18-Ejercicio8.js
+++ JS/18-Ejercicio8.js
@@ -1,2 +1,25 @@
 'use strict'
 
+//Calculadora
+
+var numero1 = parseInt(prompt("Ingresa el primer número:", 0));
+var numero2 = parseInt(prompt("Ingresa el segundo número:", 0)); 
+
+while (numero1 < 0 || numero2 < 0 || isNaN(numero2) || isNaN(numero1)) {
+    var numero1 = parseInt(prompt("Ingresa el primer número:", 0));
+    var numero2 = parseInt(prompt("Ingresa el segundo número:", 0)); 
+}
+
+var resultado = "La suma es: "+(numero1+numero2)+"<br/>"+ 
+"la resta es: "+(numero1-numero2)+"<br/>"+
+"La multiplciación es: "+(numero1*numero2)+"<br/>"+
+"La division es: "+(numero1/numero2)+"<br/>";
+
+var resultadoCMD = "La suma es: "+(numero1+numero2)+"<br/>"+ 
+"la resta es: "+(numero1-numero2)+"<br/>"+
+"La multiplciación es: "+(numero1*numero2)+"<br/>"+
+"La division es: "+(numero1/numero2)+"<br/>";
+
+document.write(resultado);
+alert(resultadoCMD);
+console.log(resultadoCMD);
