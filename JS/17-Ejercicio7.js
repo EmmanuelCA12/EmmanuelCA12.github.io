b94b2d1270bdefcf4d5e79b90a39397f06584447
EmmanuelCA12 <85908695+EmmanuelCA12@users.noreply.github.com>
2024-06-17 18:24:53 +0000

    Submit

---
+++ JS/17-Ejercicio7.js
@@ -0,0 +1,19 @@
+'use strict'
+
+//Tabla de multiplicar de un número ingresado por el usuario
+
+var numero = parseInt(prompt("¿De que número quieres la tabla? ", 1));
+
+document.write("<h1>Tabla del "+numero+"</h1>");
+for (var i = 1; i <= 10 ; i++) {
+    document.write(i+" X "+numero+" = "+(i*numero)+ "<br/>");
+    
+}
+
+//Todas las tablas de Multiplicar
+for (var j = 1; j <=10 ; j++) {
+    document.write("<h1>Tabla del "+numero+"</h1>");
+    for (var j = 1; j <= 10 ; j++) {
+        document.write(j+" X "+numero+" = "+(j*numero)+ "<br/>");
+    
+}
