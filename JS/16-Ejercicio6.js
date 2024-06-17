6f51d059843a2ba8a57aa987ee7a3e0dcc2f0b18
EmmanuelCA12 <85908695+EmmanuelCA12@users.noreply.github.com>
2024-06-17 18:04:28 +0000

    Submit

--- JS/16-Ejercicio6.js
+++ JS/16-Ejercicio6.js
@@ -1,1 +1,18 @@
 'use strict'
+
+/*
+Hacer un programa que nos diga si un número es par o impar y tiene que tener un ventana que pida el número, 
+comprobar si el número es valido, en caso contrario que pida el número otra vez.
+*/
+
+var numero = parseInt(prompt("Ingresa un número: ", 0));
+
+
+while (isNaN(numero)) {
+    numero = parseInt(prompt("Introduce un número: ", 0));
+}
+if (numero % 2 == 0) {
+    alert("Es número par");
+}else{
+    alert("Es impar");
+}
