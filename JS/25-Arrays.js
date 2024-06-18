fedf2aea2786006e8f354cfebae110d64f788e2f
EmmanuelCA12 <85908695+EmmanuelCA12@users.noreply.github.com>
2024-06-18 20:46:14 +0000

    Submit

---
+++ JS/25-Arrays.js
@@ -0,0 +1,10 @@
+'use strict'
+
+//Arrays, Arreglos, Matrices, etc.
+var nombre = "Emmanuel";
+var nombres = ["Emmanuel Calzada", "Hector Garcia", "Virginia", "Rosa Isela"];
+
+var lenguajes = new Array("PHP","JS","Go", "Java", "HTML", "Phyton");
+
+console.log(nombres);
+console.log(lenguajes);
