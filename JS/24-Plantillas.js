1d19d33373706bcb15b7500c99bb18d491a92d92
EmmanuelCA12 <85908695+EmmanuelCA12@users.noreply.github.com>
2024-06-18 20:35:24 +0000

    Submit

---
+++ JS/24-Plantillas.js
@@ -0,0 +1,8 @@
+'use strict'
+
+var nombre = prompt("Ingresa un nombre: ");
+var apellidos = prompt("Ahora tus apellidos");
+
+var texto = "Mi nombre es: "+nombre+" y mis apellidos son: "+apellidos;
+
+document.write(texto);
