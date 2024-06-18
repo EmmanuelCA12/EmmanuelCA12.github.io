718601c23d95bf6a3f9f2cd6a045580ac0218be6
EmmanuelCA12 <85908695+EmmanuelCA12@users.noreply.github.com>
2024-06-18 21:03:38 +0000

    Submit

---
+++ JS/26-Arras-multi.js
@@ -0,0 +1,8 @@
+'use strict'
+
+var categorias = ["Acción", "Terror", "Comedia", "Romance"];
+var peliculas = ["Transformes", "El Aro", "Son como niños", "La dama y el vagabundo"];
+
+var cine = [categorias, peliculas];
+
+console.log(cine);
