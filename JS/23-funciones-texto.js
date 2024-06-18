85c1204faf17274470732da436ee31d0cef1f6b2
EmmanuelCA12 <85908695+EmmanuelCA12@users.noreply.github.com>
2024-06-18 20:15:07 +0000

    Submit

---
+++ JS/23-funciones-texto.js
@@ -0,0 +1,10 @@
+'use strict'
+
+//Transformación de textos de cadenas
+
+var numero = 44;
+var texto1 = "Bienvenido al mi pagina web";
+var texto2 = "Soy la verga en todo"; 
+
+var dato = numero.toString();
+console.log(typeof dato);
