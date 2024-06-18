7299564bfaa491d9df436edc96d16aea8b504642
EmmanuelCA12 <85908695+EmmanuelCA12@users.noreply.github.com>
2024-06-18 19:59:55 +0000

    Submit

---
+++ JS/21-funciones-anonimas.js
@@ -0,0 +1,9 @@
+'use strict'
+
+//Funciones anonimas
+//Es una funcion que no tiene nombre
+
+var pelicula = function (nombre){
+    return "La pelicula es: "+nombre;
+}
+console.log(pelicula("Intensamente 2"));
