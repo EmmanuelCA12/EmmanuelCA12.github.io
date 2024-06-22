612d03406d47901c7c4792065ed9319b56c66e4f
EmmanuelCA12 <85908695+EmmanuelCA12@users.noreply.github.com>
2024-06-22 23:58:45 +0000

    Submit

--- JS/28-dom.js
+++ JS/28-dom.js
@@ -1,6 +1,13 @@
 'use strict'
 
 // dom - Document object mode
+function modificaciones(color, fondo) {
+    var color = prompt("Brinda un color para el texto");
+    var fondo = prompt("Ingresa un color para el fondo");
+    caja.style.color(color);
+    caja.style.background(fondo);
+}
 
-var caja = document.getElementById("Micaja").innerHTML;
+var caja = document.getElementById("Micaja");
+
 console.log(caja);
