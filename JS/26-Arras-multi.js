71b949f6e3a4809d1f268fbe71311e6d2df92016
EmmanuelCA12 <85908695+EmmanuelCA12@users.noreply.github.com>
2024-06-22 20:28:22 +0000

    Submit

--- JS/26-Arras-multi.js
+++ JS/26-Arras-multi.js
@@ -1,11 +1,11 @@
 'use strict'
 
-/*
+
 var categorias = ["Acción", "Terror", "Comedia", "Romance"];
 var peliculas = ["Transformes", "El Aro", "Son como niños", "La dama y el vagabundo"];
 
 var cine = [categorias, peliculas];
-*/
+
 var indice = peliculas.indexOf("King kong");
 if (indice > -1) {
     peliculas.splice(indice,1);
