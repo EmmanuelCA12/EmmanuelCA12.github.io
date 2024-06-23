4dfffcefc64bc1a0c1e96e88bac4aff7c216d785
EmmanuelCA12 <85908695+EmmanuelCA12@users.noreply.github.com>
2024-06-23 02:02:30 +0000

    Submit

--- JS/28-dom.js
+++ JS/28-dom.js
@@ -1,5 +1,6 @@
 'use strict'
 
+//Conseguir elementos con ID concreto
 // dom - Document object mode
 function modificaciones(colores, fondo) {
     var colores = prompt("Brinda un color para el texto");
@@ -12,5 +13,22 @@ function modificaciones(colores, fondo) {
 var caja = document.getElementById("Micaja");
 modificaciones();
 
+//Conseguir elemento por su etiqueta
+var todosLosDivs = document.getElementsByTagName("div");
+
+var seccion = document.querySelector(#miselección);
+var hr = document.createElement("hr");
+
+var valor;
+for(valor in todosLosDivs){
+    if (typeof todosLosDivs[valor].textContent == "string") {
+        var parrafo = document.createElement("p");
+        var texto = document.createTextNode(todosLosDivs[valor].textContent);
+        parafo.append(texto);
+        seccion.append(parrafo);
+    }
+}
+seccion.append(hr);
+//Conseguir elementos por su clase css
 
 console.log(caja);
