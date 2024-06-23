25bb401c10bcae8dd382096559ebe060ff632742
EmmanuelCA12 <85908695+EmmanuelCA12@users.noreply.github.com>
2024-06-23 00:16:09 +0000

    Submit

--- JS/28-dom.js
+++ JS/28-dom.js
@@ -4,8 +4,8 @@
 var colores = prompt("Brinda un color para el texto");
 var fondo = prompt("Ingresa un color para el fondo");
 function modificaciones(colores, fondo) {
-    caja.style.color(colores);
-    caja.style.background(fondo);
+    caja.style.color = colores;
+    caja.style.background = fondo;
 }
 modificaciones();
 var caja = document.getElementById("Micaja");
