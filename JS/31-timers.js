c501b7e94309e378a1f27f65e1dd549f34373d0e
EmmanuelCA12 <85908695+EmmanuelCA12@users.noreply.github.com>
2024-06-26 17:06:38 +0000

    Submit

--- JS/31-timers.js
+++ JS/31-timers.js
@@ -5,6 +5,13 @@ window.addEventListener('load', () =>{
     //Timers
     var tiempo = setInterval(function () {
        console.log("Set interval ejecutado"); 
-    },3000);
+
+       var Encabezado = document.querySelector("h1");
+       if (Encabezado.style.frontSize == "50px") {
+            Encabezado.style.frontSize = "20px";
+       } else {
+            Encabezado.style.frontSize = "20px";
+       }
+    },200);
 
 });
