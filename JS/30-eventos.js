70be9b9d595173b9a36b8323d0a44e28b25c20cf
EmmanuelCA12 <85908695+EmmanuelCA12@users.noreply.github.com>
2024-06-26 16:49:30 +0000

    Submit

--- JS/30-eventos.js
+++ JS/30-eventos.js
@@ -31,3 +31,25 @@ boton.addEventListener('mouseover', function () {
 boton.addEventListener('mouseout', function () {
     boton.style.background = "#ccc";
 });
+
+//Focus
+var input = document.querySelector("#Campo_nombre");
+input.addEventListener('focus', function () {
+    console.log("Estas dentro de mi");
+});
+//Blur
+input.addEventListener('blur', function () {
+    console.log("Estas fuera de mi");
+});
+//KeyDown
+input.addEventListener('keydown', function (event) {
+    console.log("Pulsaste la tecla", String.fromCharCode(event.keyCode));
+});
+//Keyprees
+input.addEventListener('keypress', function (event) {
+    console.log("Tecla precionada", String.fromCharCode(event.keyCode));
+});
+//Keyup
+input.addEventListener('keyup', function (event) {
+    console.log("Tecla soltada", String.fromCharCode(event.keyCode));
+});
