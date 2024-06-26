70ceceeabd65c6c45e0b0fe2faa52ae2be1014cb
EmmanuelCA12 <85908695+EmmanuelCA12@users.noreply.github.com>
2024-06-26 16:53:47 +0000

    Submit

--- JS/30-eventos.js
+++ JS/30-eventos.js
@@ -2,54 +2,56 @@
 
 //Eventos del ratón
 
-var boton = document.querySelector("#boton");
+window.addEventListener("load", () => {
+    var boton = document.querySelector("#boton");
 
-function cambiarcolor(){
-    console.log("Hola bebé");
-    var bg = boton.style.background;
-    if(boton == "green"){
-        boton.style.background = "red";
-    }else{
-        boton.style.background = "green";
+    function cambiarcolor(){
+        console.log("Hola bebé");
+        var bg = boton.style.background;
+        if(boton == "green"){
+            boton.style.background = "red";
+        }else{
+            boton.style.background = "green";
+        }
+        return;
     }
-    return;
-}
-
-// Click
-var boton = document.querySelector("#boton");
-boton.addEventListener('click', function () {
-    cambiarcolor();
-   // alert("¡Auch!, no tan fuerte xD");
-});
-
-//MouseOver
-boton.addEventListener('mouseover', function () {
-   boton.style.background = "yellow"; 
-});
-
-//MouseOut
-boton.addEventListener('mouseout', function () {
-    boton.style.background = "#ccc";
-});
-
-//Focus
-var input = document.querySelector("#Campo_nombre");
-input.addEventListener('focus', function () {
-    console.log("Estas dentro de mi");
-});
-//Blur
-input.addEventListener('blur', function () {
-    console.log("Estas fuera de mi");
-});
-//KeyDown
-input.addEventListener('keydown', function (event) {
-    console.log("Pulsaste la tecla", String.fromCharCode(event.keyCode));
-});
-//Keyprees
-input.addEventListener('keypress', function (event) {
-    console.log("Tecla precionada", String.fromCharCode(event.keyCode));
-});
-//Keyup
-input.addEventListener('keyup', function (event) {
-    console.log("Tecla soltada", String.fromCharCode(event.keyCode));
-});
+    
+    // Click
+    var boton = document.querySelector("#boton");
+    boton.addEventListener('click', function () {
+        cambiarcolor();
+       // alert("¡Auch!, no tan fuerte xD");
+    });
+    
+    //MouseOver
+    boton.addEventListener('mouseover', function () {
+       boton.style.background = "yellow"; 
+    });
+    
+    //MouseOut
+    boton.addEventListener('mouseout', function () {
+        boton.style.background = "#ccc";
+    });
+    
+    //Focus
+    var input = document.querySelector("#Campo_nombre");
+    input.addEventListener('focus', function () {
+        console.log("Estas dentro de mi");
+    });
+    //Blur
+    input.addEventListener('blur', function () {
+        console.log("Estas fuera de mi");
+    });
+    //KeyDown
+    input.addEventListener('keydown', function (event) {
+        console.log("Pulsaste la tecla", String.fromCharCode(event.keyCode));
+    });
+    //Keyprees
+    input.addEventListener('keypress', function (event) {
+        console.log("Tecla precionada", String.fromCharCode(event.keyCode));
+    });
+    //Keyup
+    input.addEventListener('keyup', function (event) {
+        console.log("Tecla soltada", String.fromCharCode(event.keyCode));
+    });
+}); //end load
